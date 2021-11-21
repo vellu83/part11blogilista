d90e822405590e2acf71f480b85227361b4c4054
vellu83 <velimatti.remander@gmail.com>
2021-11-21 19:20:53 +0200

    fixed healtcheck

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
+app.get('/health', (req, res) => {
+    res.send('ok')
+})
+
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
         logger.info('Connected to MongoDb')
@@ -23,6 +27,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.static('build'))
+
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
