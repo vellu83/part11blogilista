17ac4e71e1ffca442ee0c371306692cafcc8fb3a
vellu83 <velimatti.remander@gmail.com>
2021-11-21 11:43:53 +0200

    fixed conf again...

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:cypress/recommended',
-        'plugin:jest/recommended'
     ],
     'parserOptions': {
         'ecmaFeatures': { 'jsx': true },
